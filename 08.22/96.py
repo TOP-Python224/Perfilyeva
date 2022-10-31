ffc90a087081930171890001d61227dde280f2e5
Gennadiy <renardes@gmail.com>
2022-10-31 19:58:03 +0500

    08.22: правки удобства чтения кода

--- 08.22/96.py
+++ 08.22/96.py
@@ -1,4 +1,6 @@
-'''Программа запрашивает строку и расставляет в ней заглавные буквы'''
+# ИСПРАВИТЬ: разве?
+"""Программа запрашивает строку и расставляет в ней заглавные буквы"""
+
 
 def isInteger(string: str) -> str:
     str_res = ''
@@ -6,28 +8,26 @@ def isInteger(string: str) -> str:
     for i in string:
         if i != ' ':
             str_res += i
-    if str_res[0] not in ('+-0123456789'):
+    if str_res[0] not in '+-0123456789':
         flag = False
     else:        
         for i in str_res[1:]:
-            if i not in ('0123456789'):
+            if i not in '0123456789':
                 flag = False
                 break
     return flag
-    
+
+
 if __name__ == "__main__":
     str_inp = input('Введите строку > ')
-#    str_inp = 'what time do i have to be there? what`s the address? this time i`ll try to be on time!'
     if isInteger(str_inp):
         print('Введенную строку можно считать целым числом')
     else:
         print('Введенную строку нельзя считать целым числом')
 
 
+# Введите строку >   -98568
+# Введенную строку можно считать целым числом
 
-#Введите строку >   -98568
-#Введенную строку можно считать целым числом
-
-#Введите строку >   -в458
-#Введенную строку нельзя считать целым числом
-    
+# Введите строку >   -в458
+# Введенную строку нельзя считать целым числом
